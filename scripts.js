ee7d9df3e57274ef142a7a74d474480abc72ca01
Abraham Bisrat <abrahammehari0@gmail.com>
2022-02-22 03:26:29 -0600

    more cleanup and typos

--- scripts.js
+++ scripts.js
@@ -4,13 +4,11 @@ console.log("Working with GitHub api");
 function requestUserRepos(username){
     const xhr = new XMLHttpRequest();
     const url = `https://api.github.com/users/${username}/repos`;
-    // const url = `https://api.github.com/users//repos`;
     
     xhr.open('GET', url, true);
 
     // parsing through the request
     xhr.onload = function() {
-        // convert api data to JSON format
         const data = JSON.parse(this.response);
 
         console.log(data);
