3da1cbb491d4f2ac8b5a8041aacc9a3ce47f9878
Abraham Bisrat <abrahammehari0@gmail.com>
2022-02-22 21:01:03 -0600

    Added collapsible to all repo buttons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-
-console.log("Working with GitHub api");
-
 function requestUserRepos(username){
     const xhr = new XMLHttpRequest();
     const url = `https://api.github.com/users/${username}/repos`;
@@ -10,48 +7,67 @@ function requestUserRepos(username){
     // parsing through the request
     xhr.onload = function() {
         const data = JSON.parse(this.response);
-
-        console.log(data);
-
+        // console.log(data);
         const projectList = document.querySelector(".api-data");
-
         for(let i in data){
             const eachTitle = data[i].name;
             const eachDesc = data[i].description;
             const eachUrl = data[i].html_url;
             const hasPages = data[i].hasPages;
 
-            const newProject = document.createElement('a');
-            newProject.href = eachUrl;
-            newProject.innerText = eachTitle;
-            
-            if(eachDesc != null)
-                newProject.innerText += " : " + eachDesc;
+            // repo button
+            const newRepo = repoButtonMaker(eachTitle, eachDesc, projectList);
 
-            projectList.appendChild(newProject);
+            // details within the repo (collapsibles)
+            const repoContent = repoContentMaker(eachUrl);
+            
+            makeItCollapsible(newRepo);
+            projectList.appendChild(repoContent);
         }
     }
     xhr.send();
 }
-function makeItCollapsible(){
-    var coll = document.getElementsByClassName("collapsible")
-    var i;
+function repoButtonMaker(eachTitle, eachDesc, projectList) {
+    const newRepo = document.createElement('button');
+    newRepo.type = "button";
+    newRepo.className = "collapsible";
+    newRepo.innerText = eachTitle;
+    if (eachDesc != null)
+        newRepo.innerText += " : " + eachDesc;
+    projectList.appendChild(newRepo);
+    return newRepo;
+}
 
-    for(i = 0; i < coll.length; i++){
-        coll[i].addEventListener('click', function(){
-            this.classList.toggle("active");
+function repoContentMaker(eachUrl) {
+    const repoContent = document.createElement('div');
+    repoContent.className = 'repo-content';
+    const repoLink = document.createElement('a');
+    repoLink.href = eachUrl;
+    repoLink.innerText = "link to repository";
+    const trial = document.createElement('h1');
+    trial.textContent = "hellow mf!";
+    repoContent.appendChild(trial);
+    repoContent.appendChild(repoLink);
+    return repoContent;
+}
+
+function makeItCollapsible(thisOne){
+    console.log("attaching");
+    thisOne.addEventListener('click', function(){
+            thisOne.classList.toggle("active");
             var content = this.nextElementSibling;
+            
             // toggle visiblity
             if(content.style.maxHeight){
                 content.style.maxHeight = null;
-            } else{
+                console.log("clicked off");
+            } else {
                 content.style.maxHeight = content.scrollHeight + "px";
+                console.log("clicked on");
             }
-        });
-    }
+    });
 }
 // public static void main ... lol
 
 let githubUser = 'abrahammehari';
-//requestUserRepos(githubUser);
-makeItCollapsible();
+requestUserRepos(githubUser);
